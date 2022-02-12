ad1f8310bd063f467180bd61cbe4eb320f4c74af
Developer Usharani <99324598+Developer-usharani@users.noreply.github.com>
2022-02-12 16:24:31 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,49 @@
+# for i in range(0,5):
+#     print(i)
+
+
+# for i in range(10):
+#     print(i)
+
+
+# for i in "python":
+#     print(i,end=" ")
+
+
+# n = 5
+# for i in range(1,10):
+#     k = n*5
+#     print(k)
+
+
+
+
+# for i in range(10,15):
+#     print(i)
+
+
+
+
+# for i in range(15,25):
+#     if i < 18:
+#         print("your not eligible for vote")
+#     elif i >= 20:
+#         print("your eligible for vote ")
+#     else:
+#         print("false")
+
+
+
+
+# #to print a even numbers
+# list = [4,5,7,65,908,43,89]
+# for i in list:
+#     if i% 2==0:
+#         print(i)
+
+
+
+x = [5,76,87,23,32,56]
+for i in x:
+    if i>5:
+        print(i)
